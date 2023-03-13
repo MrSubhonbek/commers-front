da9bce5cce21d3ec6f6c748669097f1594d61b04
subhonbek <basubhonbek@gmail.com>
2023-03-13 11:28:24 +0300

    tailwind animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,32 @@ const colors = {
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		colors,
+		extend: {
+			keyframes: {
+				animationOpacity: {
+					from: { opacity: 0.2 },
+					to: { opacity: 1 }
+				},
+				scaleIn: {
+					'0%': {
+						opacity: 0,
+						transform: 'scale(0.9)'
+					},
+					'50%': {
+						opacity: 0.3
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'scale(1)'
+					}
+				}
+			},
+			animation: {
+				opacity: 'animationOpacity .5s ease-ii-out',
+				scaleIn: 'scaleIn .35s ease-in-out'
+			}
+		}
 	},
 	plugins: []
 }
