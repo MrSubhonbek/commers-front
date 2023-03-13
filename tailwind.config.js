f8ba391544f7d424f558839bbe40d779ac8abb5e
subhonbek <basubhonbek@gmail.com>
2023-03-13 11:20:22 +0300

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const twColors = require('tailwindcss/colors')
+
+const colors = {
+	transparent: twColors.transparent,
+	black: twColors.black,
+	white: twColors.white,
+	secondary: '#161D25',
+	primary: '#FF9902',
+	'bg-color': '#F2F2F5',
+	aqua: '#268697'
+}
+
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+}
