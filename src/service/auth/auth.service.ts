fc3f0121edd05a2cbde57147889bed194603b23e
subhonbek <basubhonbek@gmail.com>
2023-03-27 16:44:54 +0300

    add service get category

--- src/service/auth/auth.service.ts
+++ src/service/auth/auth.service.ts
@@ -2,17 +2,22 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 import { getContentType } from '@/api/api.helper'
+import { instance } from '@/api/api.interceptor'
 
 import { saveToStorage } from './auth.helper'
 import { IAuthResponse, IEmailPassword } from '@/interface/auth.interface'
 
 export const AuthService = {
 	async main(type: 'login' | 'register', data: IEmailPassword) {
-		const response = await axios<IAuthResponse>({
-			url: `/auth${type}`,
+		const response = await instance<IAuthResponse>({
+			url: `/auth/${type}`,
 			method: 'POST',
 			data
 		})
+		if (response.data.accessToken) {
+			saveToStorage(response.data)
+		}
+		return response.data
 	},
 	async getNewTokens() {
 		const refreshToken = Cookies.get('refresh')
---
+++ src/service/category.service.ts
@@ -0,0 +1,30 @@
+import { instance } from '@/api/api.interceptor'
+
+import { ICategory } from '@/interface/category.interface'
+
+export const CategoryService = {
+	async getAll() {
+		return instance<ICategory[]>({
+			url: 'category',
+			method: 'GET'
+		})
+	},
+	async getById(id: string) {
+		return instance<ICategory>({
+			url: `category/${id}`,
+			method: 'GET'
+		})
+	},
+	async getBySlug(slug: string) {
+		return instance<ICategory>({
+			url: `category/slug/${slug}`,
+			method: 'GET'
+		})
+	},
+	async create() {
+		return instance<ICategory>({
+			url: 'category',
+			method: 'POST'
+		})
+	}
+}
