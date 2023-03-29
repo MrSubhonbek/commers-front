8528e22f184f8c5e450cf13c4256c7fef9a9c909
subhonbek <basubhonbek@gmail.com>
2023-03-29 12:34:41 +0300

    add user actions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
+import { QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query/build/lib/QueryClientProvider'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { persister, store } from '@/store/store'
+
+import '@/styles/globals.css'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persister}></PersistGate>
+				<Component {...pageProps} />
+			</Provider>
+		</QueryClientProvider>
+	)
 }
