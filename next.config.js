7b508471992474e90a8c1bbad5c32bafbef5b41c
Subhonbek <basubhonbek@gmail.com>
2023-05-28 09:07:28 +0300

    front next.confix fix image domain

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
 		SERVER_URL: process.env.SERVER_URL,
 		APP_URL: process.env.APP_URL
 	},
-	images: { domains: ['loremflickr.com', 'cloudflare-ipfs.com'] }
+	images: {
+		domains: ['loremflickr.com', 'cloudflare-ipfs.com', 'drive.google.com']
+	}
 }
 
 module.exports = nextConfig
